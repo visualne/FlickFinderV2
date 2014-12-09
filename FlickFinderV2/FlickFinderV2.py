28173e4e5f20040a509c42c52f470f83eff899d3
none <none@nones-MacBook-Pro.local>
2014-12-08 19:44:20 -0500

    Changes to FlickFinder script

--- FlickFinderV2/FlickFinderV2.py
+++ FlickFinderV2/FlickFinderV2.py
@@ -1,63 +1,72 @@
 import requests
 import json
 import datetime
+import time
 
 
 #Insert your API key below
-apiKey = ''
+apiKey = 'AIzaSyApcjXnIsUwD64XkH1vfEMJSvv4ROqhOyc'
 
-#A single movie title will be added for now, but later an entire list of movies and runtimes will be used.
-movieTitle = 'The Rock'
+#A opening up our list of movies and runtimes for each movie.
+f = open('2000_MovieList','r')
 
-#Grabbing links off of page
-#r = requests.get(r'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=The+Rock&type=video&videoDuration=long&key=' + apiKey)
-r = requests.get(r'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+movieTitle+'&type=video&videoDuration=long&key=' + apiKey)
+for val in f.readlines():
+	#Splitting the line read in from in the input file into a list.
+	TitleRuntimeYear = val.split('*')
+
+	#Storing the title of the movie read in from the input file in its own variable called movieTitle
+	movieTitle = TitleRuntimeYear[0]
+	print movieTitle
+
+    #Grabbing links off of page
+	r = requests.get(r'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+movieTitle+'&type=video&videoDuration=long&key=' + apiKey)
+
+    #turning the response json key value pairs
+	data = r.json()
+
+    #Creating empty dictionary that holds the title of the video along with the id of
+    #the video
+	IDAndTitleAndLength={}
+
+    #Second API for determining the length of each of the videos
+	videoLengthsLink = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id='
+
+    #The loop below determines the title and id of the video from the above api first api that was queried with requests
+	for val in data['items']:
+		title = val['snippet']['title']
+		videoId = val['id']['videoId']
+
+		#Filling dictionary with id:title pairs ex) sf@a234asdf:'Happy Gilmore'
+		IDAndTitleAndLength[videoId] = title
+
+	#Adding list of IDs to api url
+	for key in IDAndTitleAndLength.keys():
+		videoLengthsLink =  videoLengthsLink + key + '%2C'
+
+	#Creating rest of the videoLengthsLink
+	videoLengthsLink = videoLengthsLink + '&fields=items&key=' + apiKey
 
 
-#turning the response json key value pairs
-data = r.json()
+	#Doing second search. This search will tell me the length of each of the videos in my IDAndTitleLength dictionary.
+	#I called the variable IDAndTitleLength because after the below for loop is finished the dictionary will look like this
+	#ex) sf@a234asdf:'Happy Gilmore*PT59M56S' I now have a dictionary that contains the video id of the movie found, the title of the movie
+	#and the runtime of the movie
+	r = requests.get(videoLengthsLink)
 
-#Creating empty dictionary that holds the title of the video along with the id of
-#the video
-IDAndTitleAndLength={}
+	#turning the response json key value pairs
+	data = r.json()
 
-#Second API for determining the length of each of the videos
-videoLengthsLink = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id='
-
-#The loop below determines the title and id of the video from the above api first api that was queried with requests
-for val in data['items']:
-	title = val['snippet']['title']
-	videoId = val['id']['videoId']
-
-	#Filling dictionary with id:title pairs ex) sf@a234asdf:'Happy Gilmore'
-	IDAndTitleAndLength[videoId] = title
-
-#Adding list of IDs to api url
-for key in IDAndTitleAndLength.keys():
-	videoLengthsLink =  videoLengthsLink + key + '%2C'
-
-#Creating rest of the videoLengthsLink
-videoLengthsLink = videoLengthsLink + '&fields=items&key=' + apiKey
+	#the for loop below adds the runtime of the video to the IDAndTitleAndLength dictionary
+	for val in data['items']:
+		IDAndTitleAndLength[val['id']] = IDAndTitleAndLength[val['id']] + '*' + val['contentDetails']['duration']
 
 
+	#The below data structure can now be checked against the line read in from the movie list file to see if the runtimes
+	#are close to eachother. If they are within 5 minutes of eachother print the potential match for further checking.
+	print IDAndTitleAndLength.items()
 
-#Doing second search. This search will tell me the length of each of the videos in my IDAndTitle dictionary
-#https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=Pv1nRwONwK8%2CWbs0OsZWEAc&fields=items%2FcontentDetails&key={YOUR_API_KEY}
-r = requests.get(videoLengthsLink)
+	#sleeping for 5 seconds
+	time.sleep(5)
 
-#turning the response json key value pairs
-data = r.json()
-
-#the for loop below adds the runtime of the video to the IDAndTitleAndLength dictionary
-for val in data['items']:
-		IDAndTitleAndLength[val['id']] = IDAndTitleAndLength[val['id']] + ',' + val['contentDetails']['duration']
-
-#The below data structure can now be checked against the list of movies you read into the script
-print IDAndTitleAndLength.items()
-
-##
-#Some code here to check the 2000_MovieList file against the IDAndTitleAndLength dictionary and check to see if
-#runtime are close to eachother 
-##
 
 
