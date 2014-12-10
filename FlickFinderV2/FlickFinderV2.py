f704b5653ee117e914e90d153ae6c3ced3a42f6b
none <none@nones-MacBook-Pro.local>
2014-12-09 20:57:48 -0500

    made some changes to flickfinder script

--- FlickFinderV2/FlickFinderV2.py
+++ FlickFinderV2/FlickFinderV2.py
@@ -3,9 +3,18 @@ import json
 import datetime
 import time
 
+def compareRuntimes(IDAndTitleAndLength, movieTitle, movieRuntime):
+	#Checking to see if runtimes match
+	for val in IDAndTitleAndLength.values():
+		#Grabbing the runtime only
+		runtime = val.split('*')[1][2:]
+		#Need a way to store the below time in the format HH:MM:SS
+		print runtime
+
+
 
 #Insert your API key below
-apiKey = ''
+apiKey = 'AIzaSyApcjXnIsUwD64XkH1vfEMJSvv4ROqhOyc'
 
 #A opening up our list of movies and runtimes for each movie.
 f = open('2000_MovieList','r')
@@ -16,7 +25,10 @@ for val in f.readlines():
 
 	#Storing the title of the movie read in from the input file in its own variable called movieTitle
 	movieTitle = TitleRuntimeYear[0]
-	print movieTitle
+
+	#Storing the runtime of the movies read in from the input file in its own variable called movieRuntime
+	movieRuntime = TitleRuntimeYear[1]
+	#print movieTitle + ' Runtime: ' + movieRuntime
 
     #Grabbing links off of page
 	r = requests.get(r'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+movieTitle+'&type=video&videoDuration=long&key=' + apiKey)
@@ -63,7 +75,11 @@ for val in f.readlines():
 
 	#The below data structure can now be checked against the line read in from the movie list file to see if the runtimes
 	#are close to eachother. If they are within 5 minutes of eachother print the potential match for further checking.
-	print IDAndTitleAndLength.items()
+	#print IDAndTitleAndLength.items()
+
+	#Send the above dictionary and the title and runtime of the movie read in from the input file into a function to check
+	#to see if the runtimes are close to eachother
+	compareRuntimes(IDAndTitleAndLength, movieTitle, movieRuntime)
 
 	#sleeping for 5 seconds
 	time.sleep(5)
