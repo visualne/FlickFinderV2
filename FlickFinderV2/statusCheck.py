684b2f4d101d164dc81695feda9582c97a989db0
none <none@nones-MacBook-Pro.local>
2015-02-07 17:25:38 -0500

    Added lists of movies for 2000-2005 and changed rentalCheck class to statusCheck

--- FlickFinderV2/statusCheck.py
+++ FlickFinderV2/statusCheck.py
@@ -1,10 +1,10 @@
 import requests
 import re
 
-class rentalCheck:
+class statusCheck:
 
     def __init__(self, videoID):
-        """This constructor creates the rentalCheck object 
+        """This constructor creates the statusCheck object 
 
         Args:
             videoID (str) The only parameter. This id corresponds to that of
@@ -13,7 +13,8 @@ class rentalCheck:
         self.videoID = videoID
 
     def status(self):
-        """This function returns the rental status of the video on youtube. """
+        """This function returns the status of the video on youtube. It will check to see if the movie
+        is a rental, or just some phoney link by looking for text in the html of the page"""
 
         #Full link to youtube video
         videoLink = 'http://www.youtube.com/watch?v=' + self.videoID
@@ -25,14 +26,15 @@ class rentalCheck:
         #variable.
         response = r.text
 
-        #Checking for the string 'rent or purchase' in the html
-        #if there is a match we will return true, that will mean
-        #it is a rental.
-        match = re.search('watch-checkout-offers',response)
+        #Checking for various strings in the html that would lead one to believe 
+        #the movie is a rental or just some phoney link if there is a match we will return true, 
+        #that will mean it is a rental.
+        match = re.search('watch-checkout-offers|TO WATCH FULL MOVIE|http://bit|online free full movie',response)
 
         #returning true if the above text was found in the page. This would
-        #mean that the movie is a rental.
+        #mean that the movie is a rental or a phone link of some kind.
         if match:
             return True
         else:
             return False
+
